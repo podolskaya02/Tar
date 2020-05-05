9b55180ccd61a465b5d89e9d1460a4f8414b0c39
podolskaya02 <https://github.com/podolskaya02/KotlinAsFirst2019>
2020-05-05 19:06:08 +0300

    в процессе

--- src/main/java/Tar.java
+++ src/main/java/Tar.java
@@ -1,33 +1,39 @@
-import java.io.*;
-import java.util.Arrays;
-
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+
+import java.io.*;
+import java.util.Arrays;
 
 public class Tar {
     @Option(name = "-out")
     private boolean out;
     @Option(name = "-u")
     private boolean u;
-    @Argument(required = true)
+    @Argument(required = true, hidden = true)
     private File[] files;
 
-    public static void main(String[] args) {
-        new Tar().launch(args);
+
+    private File outputFile = new File("outputFiles\\outputForUnite");
+    private BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile)); // для unite
+    private File[] outputFiles = new File("outputFiles").listFiles();
+
+    Tar() throws IOException { }
+
+
+    public static void main(String[] args) throws IOException {
+     new Tar().launch(args);
     }
 
     File launch(String[] args) {
-        CmdLineParser parser = new CmdLineParser(this);
+        CmdLineParser parser = new CmdLineParser(Tar.this);
         try {
             parser.parseArgument(args);
-            UniteFiles uniteFiles = new UniteFiles();
-            SplitUpFiles splitUpFiles = new SplitUpFiles();
             if (out) {
-               return uniteFiles.outputStream();
+               return unite();
             } else if (u) {
-                return new File(Arrays.toString(splitUpFiles.split()));
+                return new File(Arrays.toString(split()));
             }
         } catch (CmdLineException | IOException e) {
             e.printStackTrace();
@@ -35,4 +41,64 @@ public class Tar {
         return null;
     }
 
+    private StringBuilder filesNames() {
+        StringBuilder s = new StringBuilder();
+        for (File file: files) {
+            s.append(file.getName()).append(", ");
+        }
+        return s.deleteCharAt(s.length() - 2);
+    }
+
+        private File unite() throws IOException {
+        int count = 0;
+        String line;
+            for (File file: files) { // для каждого файла
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                while ((line = bufferedReader.readLine()) != null) { // считываем построчно
+                    if (line.contains("\\")) { // начальная граница
+                        while (count != 2) { // пока не дойдем до второй границы
+                            StringBuffer lineForWrite = new StringBuffer();
+                            lineForWrite.append(line);
+                            // удаляем из строки символы до первой границы
+                            if (count == 0 && line.contains("\\")){ lineForWrite.delete(0, line.indexOf("\\") + 1);
+                            count = count + 1;}
+                            // удаляем из строки символы после второй границы
+                             else if (count == 1 && line.contains("\\") ){ lineForWrite.delete(line.indexOf("\\"), line.length());
+                            count = count + 1; }
+                           // если вторая граница находится на той же строке, что и первая - удаляем всё, что после неё
+                            if (lineForWrite.toString().contains("\\")) { lineForWrite.delete(lineForWrite.indexOf("\\"), line.length());
+                            count = count + 1; }
+                            else { line = bufferedReader.readLine();} // иначе считываем следующую строку
+                        bufferedWriter.write(String.valueOf(lineForWrite));
+                        bufferedWriter.flush();
+                        bufferedWriter.write("\n");
+                    }
+                        count = 0;
+                    }
+                }
+            }
+            bufferedWriter.newLine();
+            bufferedWriter.write("Input files: " + filesNames());
+            bufferedWriter.close();
+            return outputFile;
+    }
+
+        private File[] split() throws IOException {
+            boolean mark;
+            String line;
+            for (File file : outputFiles) { // записываем данные в выходные файлы
+                mark = false;
+                BufferedWriter bufferedWritter = new BufferedWriter(new FileWriter(file));
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(String.valueOf(files)));
+                while (!mark) {
+                    line = bufferedReader.readLine();
+                    if (line == null) break;
+                    bufferedWritter.write(line);
+                    bufferedWritter.flush();
+                    bufferedWritter.write("\\");
+                    if (line.endsWith("\\")) mark = true; // граница для разделения файла
+                }
+            }
+            return outputFiles;
+        }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,20 +13,23 @@ class Tests {
     private File outputFile2 = new File("outputFiles\\out2");
     private File outputFile3 = new File("outputFiles\\outputForUnite");
 
+    Tests() throws IOException {
+    }
+
     @Test
     void test1() {
-        assertEquals(outputFile1, tar.launch(new String[]{"file1", "file2", "-out"}));
+        assertEquals(outputFile3 , tar.launch(new String[]{"inputFiles/file1", "inputFiles/file2", "-out"}));
     }
 
     @Test
     void test2() {
-        assertEquals(outputFile3, tar.launch(new String[]{"file1", "file2", "file3", "-out"}));
+        assertEquals(outputFile3, tar.launch(new String[]{"inputFiles/file1", "inputFiles/file2", "inputFiles/file3", "-out"}));
     }
 
     @Test
     void test3() {
         File array = new File(Arrays.toString( new File[] {outputFile1, outputFile2}));
-        assertEquals(array, tar.launch(new String[]{"file1", "-u"}));
+        assertEquals(array, tar.launch(new String[]{"inputFiles/file1", "-u"}));
     }
-    
+
 }
