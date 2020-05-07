b83ce1b81495bf0af2faed7ac227b8e50d5c5ae5
podolskaya02 <https://github.com/podolskaya02/KotlinAsFirst2019>
2020-05-07 14:31:54 +0300

    commit

--- src/main/java/Tar.java
+++ src/main/java/Tar.java
@@ -14,24 +14,16 @@ public class Tar {
     @Argument(required = true, hidden = true)
     private File[] files;
 
-
-    private File outputFile = new File("outputFiles\\outputForUnite");
-    private BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile)); // для unite
-    private File[] outputFiles = new File("outputFiles").listFiles();
-
-    Tar() throws IOException { }
-
-
     public static void main(String[] args) throws IOException {
-     new Tar().launch(args);
+        new Tar().launch(args);
     }
 
-    File launch(String[] args) {
+    File launch(String[] args) throws FileNotFoundException {
         CmdLineParser parser = new CmdLineParser(Tar.this);
         try {
             parser.parseArgument(args);
             if (out) {
-               return unite();
+                return unite();
             } else if (u) {
                 return new File(Arrays.toString(split()));
             }
@@ -43,62 +35,86 @@ public class Tar {
 
     private StringBuilder filesNames() {
         StringBuilder s = new StringBuilder();
-        for (File file: files) {
+        for (File file : files) {
             s.append(file.getName()).append(", ");
         }
         return s.deleteCharAt(s.length() - 2);
     }
 
-        private File unite() throws IOException {
+    private File unite() throws IOException {
+        File outputFile = new File("outputForUnite\\outputForUnite"); // выходной файл после объединения файлов
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
         int count = 0;
         String line;
-            for (File file: files) { // для каждого файла
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                while ((line = bufferedReader.readLine()) != null) { // считываем построчно
-                    if (line.contains("\\")) { // начальная граница
-                        while (count != 2) { // пока не дойдем до второй границы
-                            StringBuffer lineForWrite = new StringBuffer();
-                            lineForWrite.append(line);
-                            // удаляем из строки символы до первой границы
-                            if (count == 0 && line.contains("\\")){ lineForWrite.delete(0, line.indexOf("\\") + 1);
-                            count = count + 1;}
-                            // удаляем из строки символы после второй границы
-                             else if (count == 1 && line.contains("\\") ){ lineForWrite.delete(line.indexOf("\\"), line.length());
-                            count = count + 1; }
-                           // если вторая граница находится на той же строке, что и первая - удаляем всё, что после неё
-                            if (lineForWrite.toString().contains("\\")) { lineForWrite.delete(lineForWrite.indexOf("\\"), line.length());
-                            count = count + 1; }
-                            else { line = bufferedReader.readLine();} // иначе считываем следующую строку
+        for (File file : files) { // для каждого файла
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            while ((line = bufferedReader.readLine()) != null) { // считываем построчно
+                if (line.contains("\\")) { // начальная граница
+                    while (count != 2) { // пока не дойдем до второй границы
+                        StringBuffer lineForWrite = new StringBuffer();
+                        lineForWrite.append(line);
+                        // удаляем из строки символы до первой границы
+                        if (count == 0 && line.contains("\\")) {
+                            lineForWrite.delete(0, line.indexOf("\\") + 1);
+                            count = count + 1;
+                        }
+                        // удаляем из строки символы после второй границы
+                        else if (count == 1 && line.contains("\\")) {
+                            lineForWrite.delete(line.indexOf("\\"), line.length());
+                            count = count + 1;
+                        }
+                        // если вторая граница находится на той же строке, что и первая - удаляем всё, что после неё
+                        if (lineForWrite.toString().contains("\\")) {
+                            lineForWrite.delete(lineForWrite.indexOf("\\"), line.length());
+                            count = count + 1;
+                        } else {
+                            line = bufferedReader.readLine();
+                        } // иначе считываем следующую строку
                         bufferedWriter.write(String.valueOf(lineForWrite));
                         bufferedWriter.flush();
                         bufferedWriter.write("\n");
                     }
-                        count = 0;
-                    }
+                    count = 0;
                 }
             }
-            bufferedWriter.newLine();
-            bufferedWriter.write("Input files: " + filesNames());
-            bufferedWriter.close();
-            return outputFile;
+        }
+        bufferedWriter.newLine();
+        bufferedWriter.write("Input files: " + filesNames());
+        bufferedWriter.close();
+        return outputFile;
     }
 
-        private File[] split() throws IOException {
-            boolean mark;
-            String line;
-            for (File file : outputFiles) { // записываем данные в выходные файлы
-                mark = false;
-                BufferedWriter bufferedWritter = new BufferedWriter(new FileWriter(file));
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(String.valueOf(files)));
-                while (!mark) {
-                    line = bufferedReader.readLine();
-                    if (line == null) break;
-                    bufferedWritter.write(line);
-                    bufferedWritter.flush();
-                    bufferedWritter.write("\\");
-                    if (line.endsWith("\\")) mark = true; // граница для разделения файла
-                }
+    private File[] split() throws IOException {
+        File[] outputFiles = new File("outputFiles").listFiles(); // выходные файлы после разделения файла
+        boolean mark;
+        String line;
+        StringBuilder lineForWrite;
+        StringBuilder anotherLine = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(String.valueOf(files[0]))));
+        assert outputFiles != null;
+        for (File file : outputFiles) {
+            mark = false;
+            BufferedWriter bufferedWritter = new BufferedWriter(new FileWriter(file));
+            if (!anotherLine.toString().equals("")) {
+                bufferedWritter.write(String.valueOf(anotherLine)); // записываем значение строчки после границы в новый файл
+                bufferedWritter.write("\n");
             }
-            return outputFiles;
+            anotherLine = new StringBuilder(); // обнуляем
+            while ((line = bufferedReader.readLine()) != null) { // пока не дойдем до границы разделения
+                    lineForWrite = new StringBuilder();
+                    lineForWrite.append(line);
+                    if (line.contains("\\")) {
+                        anotherLine.append(lineForWrite);
+                        anotherLine.delete(0, line.indexOf("\\") + 1); // сохраняем конец строчки после границы
+                        lineForWrite.delete(line.indexOf("\\"), line.length()); // удаляем всё, что после границы
+                        mark = true;
+                    }
+                    bufferedWritter.write(String.valueOf(lineForWrite));
+                    bufferedWritter.flush();
+                    if (mark) break;
+                    bufferedWritter.write("\n");
+            }
         }
+        return outputFiles;
+    }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -11,25 +12,25 @@ class Tests {
     private Tar tar = new Tar();
     private File outputFile1 = new File("outputFiles\\out");
     private File outputFile2 = new File("outputFiles\\out2");
-    private File outputFile3 = new File("outputFiles\\outputForUnite");
+    private File outputFile3 = new File("outputForUnite\\outputForUnite");
 
     Tests() throws IOException {
     }
 
     @Test
-    void test1() {
-        assertEquals(outputFile3 , tar.launch(new String[]{"inputFiles/file1", "inputFiles/file2", "-out"}));
+    void test1() throws FileNotFoundException {
+        assertEquals(outputFile3 , tar.launch(new String[]{"inputFiles\\file1", "inputFiles\\file2", "-out"}));
     }
 
     @Test
-    void test2() {
-        assertEquals(outputFile3, tar.launch(new String[]{"inputFiles/file1", "inputFiles/file2", "inputFiles/file3", "-out"}));
+    void test2() throws FileNotFoundException {
+        assertEquals(outputFile3, tar.launch(new String[]{"inputFiles\\file1", "inputFiles\\file2", "inputFiles\\file3", "-out"}));
     }
 
     @Test
-    void test3() {
+    void test3() throws FileNotFoundException {
         File array = new File(Arrays.toString( new File[] {outputFile1, outputFile2}));
-        assertEquals(array, tar.launch(new String[]{"inputFiles/file1", "-u"}));
+        assertEquals(array, tar.launch(new String[]{"inputFiles\\file1", "-u"}));
     }
 
 }
